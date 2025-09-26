6b283b2b4fc64f2d27b8e6475262c6189ac6ec44
Mauschelbär <marc.manhart@mailbox.org>
2025-09-26 16:25:35 +0200

    -

--- modules/i2p.nix
+++ modules/i2p.nix
@@ -6,13 +6,19 @@
     bandwidth = 4096;
     port = 4567;
     enableIPv4 = true;
-    enableIPv6 = true;
-    floodfill = true;
+    enableIPv6 = false;
+    floodfill = false;
     proto = {
       http = {
         enable = true;
         address = "127.0.0.1";
         port = 7070;
+      };
+      httpProxy = {
+        enable = true;
+        address = "127.0.0.1";
+        port = 4444;
+        # optional: outproxy = "false";  # nur .i2p, kein clearnet via Outproxy
       };
       socksProxy = {
         enable = true;
